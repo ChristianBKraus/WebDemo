dd1abc89bf25df453986f85b5043278f17aa8154
Kraus <christian.kraus@sap.com>
2018-04-30 20:17:04 +0200

    Corrected static content

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -4,10 +4,11 @@ app.controller('Ctrl', function($scope, $http) {
 
 	$scope.personen = [];
 	$scope.new_person = {};
+	$scope.test = { name: "Hello" };
 			 			 			 
 	 $scope.readPersonen = function() {
 		 console.log("Lesen von Personen");
-	     $http.get('api/personen' )
+	     $http.get('/api/person' )
 			  .then(  function(response) {
 			  		console.log("Personen wurden gelesen");
 			    	$scope.personen = response.data;
@@ -15,21 +16,22 @@ app.controller('Ctrl', function($scope, $http) {
 					$scope.personen.forEach(function(el){
 						console.log(el);
 					});
+					console.log( $scope.personen );
 			  });
 	};
-	$scope.readResonen();
+	$scope.readPersonen();
 
     $scope.add = function() {
 		console.log("Hinzufuegen von Person");
 		$http.post('api/person')
 		     .then( function success(response) {
 		    			console.log("Person hinzugefuegt");
-		    			$scope.read();
+		    			$scope.readPersonen();
 		    		}, function error(response) {
 		    	      	console.log("Fehler beim hinzufuegen von Personen");
 		    	      	response.data.errors.forEach(function(e) {
 		    	    	  	console.log(e);
 		    	      	})
 		    	   });
-	}
+	};
 });		
