2318ffde1aa6201ce5c93bdefa58bbc2f9256275
Kraus <christian.kraus@sap.com>
2018-04-30 15:28:58 +0200

    Controller

---
+++ src/main/java/jupiterpa/webdemo/DemoController.java
@@ -0,0 +1,21 @@
+package jupiterpa.webdemo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping(path = DemoController.PATH)
+@RestController
+public class DemoController {
+    public static final String PATH ="/demo";
+    
+    @GetMapping("/test/{name}")
+    public String get(@PathVariable String name) { 
+        System.out.println("GET Hello " + name); 
+    	return "Hello " + name;
+    }
+    
+    @PostMapping("/test/{name}")
+    public void post(@PathVariable String name) {
+        System.out.println("POST Hello " + name);
+    }
+
+}
